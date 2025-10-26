f2130d3320c54f1ec58b1794837008141f56b773
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2025-10-27 05:19:15 +0800

    Update adventofcode.com:2025:day:1.lisp
    Solutions for https://adventofcode.com/2015/day/1

--- adventofcode.com:2025:day:1.lisp
+++ adventofcode.com:2025:day:1.lisp
@@ -2,4 +2,12 @@
 (defun adventofcode.com/2015/day/1 (&optional (pathname adventofcode.com/2015/day/1/input.txt))
  (apply '+ (with-open-file (stream pathname)
             (loop for c = (read-char stream nil :eof) until (eq c :eof) collect (case c (#\( 1) (#\) -1))))))
-;; Call (adventofcode.com/2015/day/1) with a default file with pathname "adventofcode.com:2015:day:1:input.txt" to find the answer.
+;; Call (adventofcode.com/2015/day/1) with a file with the default pathname "adventofcode.com:2015:day:1:input.txt" to find the answer.
+
+(defun adventofcode.com/2015/day/1/Part/II (&optional (pathname adventofcode.com/2015/day/1/input.txt))
+ (let ((collection nil))
+      (with-open-file (stream pathname)
+       (loop for c = (read-char stream nil :eof) until (eq -1 (apply '+ collection))
+             collect (setq collection (cons (case c (#\( 1) (#\) -1)) collection))))
+      (length collection)))
+;; Call (adventofcode.com/2015/day/1/Part/II) with a file with the default pathname "adventofcode.com:2015:day:1:input.txt" to find the answer.
